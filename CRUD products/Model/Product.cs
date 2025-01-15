2485ed3178c626fcf176920660acb946e6db1973
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-15 10:58:34 -0500

    add product model

---
+++ CRUD products/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_products.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string  Name { get; set; }
+        public string? Description { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Stock can´t be negative ")] public int Stock { get; set; }
+    }
+}
