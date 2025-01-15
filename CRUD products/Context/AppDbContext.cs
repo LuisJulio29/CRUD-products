5fdcdc7e740235d9ba4a6f22982a2c0c611f671c
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-15 11:40:33 -0500

    add Bd Context

---
+++ CRUD products/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUD_products.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_products.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        public DbSet<Product> Products { get; set; }
+    }
+}
